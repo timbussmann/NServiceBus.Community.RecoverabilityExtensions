9c1acbb9487f076d043a0de14cb692a27812f11a
Tim Bussmann <timbussmann@users.noreply.github.com>
2023-01-17 22:26:34 +0100

    remove todo comment

--- NServiceBus.Community.RecoverabilityExtensions/HandlerTrackingBehavior.cs
+++ NServiceBus.Community.RecoverabilityExtensions/HandlerTrackingBehavior.cs
@@ -15,7 +15,6 @@ class HandlerTrackingBehavior : Behavior<IInvokeHandlerContext>
         catch (Exception e)
         {
             // This is one of the few possibilities to float data from the pipeline to the error context
-            //TODO: this approach only takes the failing handler's config into account. A more sophisticated approach should take into account the config of all invoked handlers so far and select the "lowest" applicable configuration.
             var handlerType = context.MessageHandler.HandlerType;
             e.Data.Add(HandlerTypeExceptionDataKey, handlerType);
             throw;
