2831a2ff09676ee7ae86e027f1526d448dfb2329
Tim Bussmann <timbussmann@users.noreply.github.com>
2023-01-17 22:24:03 +0100

    add acceptance tests (#1)

---
+++ RecoverabilityExtensions.AcceptanceTests/ServerWithoutRetries.cs
@@ -0,0 +1,28 @@
+﻿using NServiceBus;
+using NServiceBus.AcceptanceTesting.Support;
+using NServiceBus.Community.RecoverabilityExtensions;
+
+namespace RecoverabilityExtensions.AcceptanceTests;
+
+class ServerWithoutRetries : IEndpointSetupTemplate
+{
+    public async Task<EndpointConfiguration> GetConfiguration(RunDescriptor runDescriptor, EndpointCustomizationConfiguration endpointConfiguration,
+        Func<EndpointConfiguration, Task> configurationBuilderCustomization)
+    {
+        var configuration = new EndpointConfiguration(endpointConfiguration.EndpointName);
+
+        configuration.UseTransport<LearningTransport>();
+        configuration.UsePersistence<LearningPersistence>();
+
+        configuration.EnableAttributeAwareRecoverabilityPolicy();
+
+        configuration.PurgeOnStartup(true);
+        configuration.Recoverability()
+            .Immediate(i => i.NumberOfRetries(0))
+            .Delayed(d => d.NumberOfRetries(0));
+
+        await configurationBuilderCustomization(configuration);
+
+        return configuration;
+    }
+}
